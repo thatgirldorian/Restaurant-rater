8fb600afd9c61355c6281cbd0f166171336ef5c0
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-04-11 23:38:28 +0400

    added a search functionality via the API

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 //Use the axios library to make HTTP requests with a helper function to the API
 const fetchData = async () => {
-    const response = await axios.get('https://api.aniapi.com')
-
+    console.log('Start fetching data')
+    const response = await axios.get("https://api.aniapi.com/v1/anime", {
+        headers: {
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjE1NzUiLCJuYmYiOjE2NDk1MzExMzYsImV4cCI6MTY1MjEyMzEzNiwiaWF0IjoxNjQ5NTMxMTM2fQ.6EnZs5eZcnThqtd6HUW7xM_Ysm-COK6ubyKyxzYdahw',
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        params: {
+            title: "immortal king"
+        }
+    })
     //show request
     console.log(response.data)
 }
