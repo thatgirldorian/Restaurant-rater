99c5bffee4d0a25de58f825ea919e722b0ed7d86
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-04-12 23:04:00 +0400

    added a way to show searched anime

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Use the axios library to make HTTP requests with a helper function to the API
-const fetchData = async () => {
+const fetchData = async (searchedAnime) => {
     console.log('Start fetching data')
     const response = await axios.get("https://api.aniapi.com/v1/anime", {
         headers: {
@@ -8,12 +8,33 @@ const fetchData = async () => {
             'Accept': 'application/json'
         },
         params: {
-            title: "immortal king"
+            title: searchedAnime
         }
     })
     //show request
-    console.log(response.data)
+    let animeData = response.data.data.documents 
+    return animeData
 }
 
-//Call helper function
-fetchData()
+
+const input = document.querySelector('input')
+
+//Select the input element and add event listeners to find a show when text is added
+const onInput = async event => {
+        const animes = await fetchData(event.target.value)
+        console.log(animes)
+
+        // get each anime
+        for (let anime of animes) {
+            const div = document.createElement('div')
+
+            div.innerHTML = `
+            <img src="${anime.banner_image}" />
+            <h2>${anime.titles.en}</h2>
+            <p>${anime.descriptions.en}</p>
+            `
+            document.querySelector('#target').appendChild(div)
+        }
+}
+    //get the value of whatever is typed, but set timeout for unnecessary requests
+    input.addEventListener('input', debounce(onInput, 500))
