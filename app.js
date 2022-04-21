6ce0007386c3646925e6f3569a2ab385a109d7f4
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-04-22 00:24:16 +0400

    completely refactored the autocomplete function

--- app.js
+++ app.js
@@ -1,89 +1,57 @@
 //Use the axios library to make HTTP requests with a helper function to the API
-const fetchData = async searchedAnime => {
-    console.log('Start fetching data')
-    const response = await axios.get("https://api.aniapi.com/v1/anime", {
-        headers: {
-            'Authorization': 'Bearer ****',
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        params: {
-            title: searchedAnime
-        }
-    })
 
-
-    //show request and handle errors for when we we search for non-existent anime
-    let animeData = response.data.data.documents 
-        if (response.data.message === "Zero anime found") {
-            return []
-        } else
-    return animeData
-}
-
-//set up widget autocomplete function 
-const root = document.querySelector('.autocomplete')
-root.innerHTML = `
-    <label><b>Search for a new anime below:</b></label>
-    <input class="input" />
-        <div class="dropdown">
-            <div class="dropdown-menu">
-                <div class="dropdown-content results"></div>
-            </div>
-        </div>
-`
-
-
-const input = document.querySelector('input')
-const dropdown = document.querySelector('.dropdown')
-const resultsWrapper = document.querySelector('.results')
-
-
-//Select the input element and add event listeners to find a show when text is added
-const onInput = async event => {
-        const animes = await fetchData(event.target.value)
-        // close dropdown if our search does not show any show at all
-        if (!event.target.value) {
-            dropdown.classList.remove('is-active')
-            return;
-        }
-
-
-        //handle broken images
-        resultsWrapper.innerHTML = ' '
-        //open up a dropdown to add movies
-        dropdown.classList.add('is-active')
-        // get each anime
-        for (let anime of animes) {
-            const animeOption = document.createElement('a')
-            const imgSrc = anime.cover_image || anime.banner_image;
-
-            animeOption.classList.add('dropdown-item')
-            animeOption.innerHTML = `
-            <img src="${imgSrc}" /> 
-            ${anime.titles.en}
-            `
-
-            //handling anime selection
-            animeOption.addEventListener('click', () => {
-                dropdown.classList.remove('is-active')
-                input.value = anime.titles.en
-                //make a follow-up request to show anime info with a helper function
-                onAnimeSelect(anime)
+//call the autocomplete function
+createAutoComplete ({
+    root: document.querySelector('.autocomplete'),
+    //render an individual anime object
+    renderAnimeOption(anime) {
+        const imgSrc = anime.cover_image || anime.banner_image;
+        return `
+                    <img src="${imgSrc}" /> 
+                    ${anime.titles.en}
+                    `
+    },
+    onOptionSelect(anime) {
+        onAnimeSelect(anime);
+    }, 
+    inputValue(anime) {
+        return anime.titles.en
+    }, 
+    async fetchData(searchedAnime) {
+            console.log('Start fetching data')
+            const response = await axios.get("https://api.aniapi.com/v1/anime", {
+                headers: {
+                    'Authorization': 'Bearer ****',
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                params: {
+                    title: searchedAnime
+                }
             })
-
-            resultsWrapper.appendChild(animeOption)
+        
+        
+            //show request and handle errors for when we we search for non-existent anime
+            let animeData = response.data.data.documents 
+                if (response.data.message === "Zero anime found") {
+                    return []
+                } else
+            return animeData
         }
-}
-    //get the value of whatever is typed, but set timeout for unnecessary requests
-    input.addEventListener('input', debounce(onInput, 500))
+})
 
-    //automatically close the dropdown 
-    document.addEventListener('click', event => {
-        if (!root.contains(event.target)) {
-            dropdown.classList.remove('is-active')
-        }
-    })
+//for the second anime box on the right
+createAutoComplete ({
+    root: document.querySelector('.autocomplete-two'),
+    //render an individual anime object
+    renderAnimeOption(anime) {
+        const imgSrc = anime.cover_image || anime.banner_image;
+        return `
+                    <img src="${imgSrc}" /> 
+                    ${anime.titles.en}
+                    `
+    }
+})
 
     const onAnimeSelect = async anime => {
         const response = await axios.get("https://api.aniapi.com/v1/anime/", {
--- autocomplete.js
+++ autocomplete.js
@@ -1,4 +1,69 @@
 //create a reusable autocomplete function
-const createAutoComplete = (config) => {
+const createAutoComplete = ({ 
+    //destructure out the separate functions
+    root, 
+    renderAnimeOption, 
+    onOptionSelect, 
+    inputValue, 
+    fetchData }) => {
+    //set up widget autocomplete function 
+        root.innerHTML = `
+            <label><b>Search for a new anime below:</b></label>
+            <input class="input" />
+                <div class="dropdown">
+                    <div class="dropdown-menu">
+                        <div class="dropdown-content results"></div>
+                    </div>
+                </div>
+        `
+
+
+        const input = root.querySelector('input')
+        const dropdown = root.querySelector('.dropdown')
+        const resultsWrapper = root.querySelector('.results')
+
+
+        //Select the input element and add event listeners to find a show when text is added
+        const onInput = async event => {
+                const items = await fetchData(event.target.value)
+                // close dropdown if our search does not show any show at all
+                if (!event.target.value) {
+                    dropdown.classList.remove('is-active')
+                    return;
+                }
+
+
+                //handle broken images
+                resultsWrapper.innerHTML = ' '
+                //open up a dropdown to add movies
+                dropdown.classList.add('is-active')
+                // get each anime
+                for (let item of items) {
+                    const animeOption = document.createElement('a')
+                    
+
+                    animeOption.classList.add('dropdown-item')
+                    animeOption.innerHTML = renderAnimeOption(item)
+
+                    //handling anime selection
+                    animeOption.addEventListener('click', () => {
+                        dropdown.classList.remove('is-active')
+                        input.value = inputValue(item)
+                        //make a follow-up request to show anime info with a helper function
+                        onOptionSelect(item)
+                    })
+
+                    resultsWrapper.appendChild(animeOption)
+                }
+        }
+            //get the value of whatever is typed, but set timeout for unnecessary requests
+            input.addEventListener('input', debounce(onInput, 500))
+
+            //automatically close the dropdown 
+            document.addEventListener('click', event => {
+                if (!root.contains(event.target)) {
+                    dropdown.classList.remove('is-active')
+                }
+            })
 
 }
